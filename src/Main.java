bc84f09b2ccf04e8a46f072737eb56da77174f83
sooyeon-kr <sooyeon7027@naver.com>
2024-07-04 16:46:34 +0900

    Update: 변경된 코드에 맞게, 객체 생성 코드 변경

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Phone apple = new Phone("애플", "@", "잡스");
-        Phone samsong = new Phone("삼송", "*", "재용");
-        apple.buy();
-        apple.turnOn();
 
-        samsong.buy();
-        samsong.turnOn();
+        Galaxy galaxy23 = new Galaxy("삼송");
+        Person jaeyong = new Person("재용");
+        jaeyong.buyPhone(galaxy23);
+        jaeyong.turnOnPhone();
+
+        Apple apple = new Apple("애플");
+        Person jobs = new Person("잡스");
+        jobs.buyPhone(apple);
+        jobs.turnOnPhone();
     }
 }
